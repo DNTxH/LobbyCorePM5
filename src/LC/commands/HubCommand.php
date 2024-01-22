28007a02e797543e4bb764464fb108416802bc51
DNTxH <101135927+DNTxH@users.noreply.github.com>
2024-01-22 17:46:38 +0300

    Done

--- src/LC/commands/HubCommand.php
+++ src/LC/commands/HubCommand.php
@@ -6,6 +6,7 @@ use pocketmine\entity\object\ItemEntity;
 use pocketmine\item\ItemIdentifier;
 use pocketmine\item\ItemTypeIds;
 use pocketmine\item\VanillaItems;
+use pocketmine\block\VanillaBlocks;
 use pocketmine\player\Player;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
@@ -38,19 +39,29 @@ class HubCommand extends Command
                 $player->getArmorInventory()->clearALL();
                 $player->sendMessage(str_replace(["{player}"], [$player->getName()], $this->plugin->getConfig()->get("Hub-Message")));
 
-                $item1 = VanillaItems::COMPASS();
-                $item1->setCustomName("Games");
+                $item1 = VanillaBlocks::ENDER_CHEST()->asItem();
+                $item1->setCustomName("Cosmetics");
 
-                $item2 = VanillaItems::DIAMOND_AXE();
-                $item2->setCustomName("Cosmeticos");
+                $item2 = VanillaBlocks::ANVIL()->asItem();
+                $item2->setCustomName("ReportPlayer");
 
                 $item3 = VanillaItems::BOOK();
                 $item3->setCustomName("Informacion");
+                
+                $item3 = VanillaItems::COMPASS();
+                $item3->setCustomName("Teleporter");
 
+                $item4 = VanillaItems::POPPED_CHORUS_FRUIT();
+                $item4->setCustomName("InfoUI");
+
+                $item5 = VanillaItems::NETHER_STAR();
+                $item5->setCustomName("Lobby");
 
                 $player->getInventory()->setItem(0, $item1);
-                $player->getInventory()->setItem(4, $item2);
-                $player->getInventory()->setItem(8, $item3);
+                $player->getInventory()->setItem(1, $item2);
+                $player->getInventory()->setItem(4, $item3);
+                $player->getInventory()->setItem(7, $item4);
+                $player->getInventory()->setItem(8, $item5);
             }
         }
     }
