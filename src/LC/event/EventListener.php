c8f56cb5ff39e26afe8cf3e680a3b77c02d744fd
DNTxH <101135927+DNTxH@users.noreply.github.com>
2024-01-22 17:41:11 +0300

    Done

--- src/LC/event/EventListener.php
+++ src/LC/event/EventListener.php
@@ -41,19 +41,26 @@ class EventListener implements Listener
         Server::getInstance()->broadcastMessage(str_replace(["{player}"], [$player->getName()], $this->plugin->getConfig()->get("Join-Message")));
         $player->teleport(Server::getInstance()->getWorldManager()->getDefaultWorld()->getSafeSpawn());
 
-        $item1 = VanillaItems::COMPASS();
-        $item1->setCustomName("Games");
+        $item1 = VanillaBlocks::ENDER_CHEST()->asItem();
+        $item1->setCustomName("Cosmetics");
 
-        $item2 = VanillaItems::DIAMOND_AXE();
-        $item2->setCustomName("Cosmeticos");
+        $item2 = VanillaBlocks::ANVIL()->asItem();
+        $item2->setCustomName("ReportPlayer");
 
-        $item3 = VanillaItems::BOOK();
-        $item3->setCustomName("Informacion");
+        $item3 = VanillaItems::COMPASS();
+        $item3->setCustomName("Teleporter");
 
+        $item4 = VanillaItems::POPPED_CHORUS_FRUIT();
+        $item4->setCustomName("InfoUI");
+
+        $item5 = VanillaItems::NETHER_STAR();
+        $item5->setCustomName("Lobby");
 
         $player->getInventory()->setItem(0, $item1);
-        $player->getInventory()->setItem(4, $item2);
-        $player->getInventory()->setItem(8, $item3);
+        $player->getInventory()->setItem(1, $item2);
+        $player->getInventory()->setItem(4, $item3);
+        $player->getInventory()->setItem(7, $item4);
+        $player->getInventory()->setItem(8, $item5);
     }
 
     public function onQuit(PlayerQuitEvent $event){
@@ -65,18 +72,22 @@ class EventListener implements Listener
         Server::getInstance()->broadcastMessage(str_replace(["{player}"], [$player->getName()], $this->plugin->getConfig()->get("Quit-Message")));
     }
 	
-    public function onClick(PlayerInteractEvent $event)
-    {
+    public function onClick(PlayerInteractEvent $event){
         $player = $event->getPlayer();
         $itn = $player->getInventory()->getItemInHand()->getCustomName();
-        if ($itn == "Games") {
-            LobbyCore::getInstance()->getUI()->getGames($player);
-        }
-        if ($itn == "Cosmeticos") {
+        if ($itn == "Cosmetics") {
             LobbyCore::getInstance()->getUI()->getCosmetics($player);
         }
-        if ($itn == "Informacion") {
+	if ($itn == "ReportPlayer"){
+            $this->plugin->getServer()->getCommandMap()->dispatch($player, "report");
+        }
+	if ($itn == "Teleporter") {
+            LobbyCore::getInstance()->getUI()->getGames($player);
+        }
+        if ($itn == "InfoUI") {
             LobbyCore::getInstance()->getUI()->getInfo($player);
         }
+	if ($itn == "Lobby"){
+            $this->plugin->getServer()->getCommandMap()->dispatch($player, "hub");
+        }
     }
-}
